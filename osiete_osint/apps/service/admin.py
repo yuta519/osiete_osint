5405409178d67c530d997f8ef6de9b11321ea60d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-13 22:30:58 +0900

    add new table class VtSummary at admin.py

--- osiete_osint/apps/service/admin.py
+++ osiete_osint/apps/service/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import DataList, OsintSearchHistory, Service
+from .models import DataList, OsintSearchHistory, Service, VtSummary
 
 # Register your models here.
 
 admin.site.register(DataList)
 admin.site.register(OsintSearchHistory)
 admin.site.register(Service)
+admin.site.register(VtSummary)
+
