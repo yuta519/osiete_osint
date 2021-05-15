628b9ca9b753360307f18d070e1c0c29f83507b0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-15 17:17:01 +0900

    add VtComments

--- osiete_osint/apps/service/admin.py
+++ osiete_osint/apps/service/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import DataList, OsintSearchHistory, Service, VtSummary
+from .models import (DataList, OsintSearchHistory, Service, VtComments, 
+                    VtSummary,)
 
 # Register your models here.
 
 admin.site.register(DataList)
 admin.site.register(OsintSearchHistory)
 admin.site.register(Service)
+admin.site.register(VtComments)
 admin.site.register(VtSummary)
-
