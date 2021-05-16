7b52b8917f0ec9ae5f3b24491538124908578e3b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-16 21:54:08 +0900

    add new file urlscan for cli

---
+++ osiete_osint/apps/service/management/commands/urlscan.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from osiete_osint.apps.service.client import UrlScanClient
+
+
+class Command(BaseCommand):
+    help = 'Return UrlScan results on console'
+
+    def add_arguments(self, parser):
+        parser.add_argument('targets', nargs='+', type=str)
+
+    def handle(self, *args, **kwargs):
+        urlscan = UrlScanClient()
+        for target in kwargs['targets']:
+            response = urlscan.fetch_domain_detail(target)
+            print(response.text)
