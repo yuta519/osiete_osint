b49170f395d64f38800caf4aea9f894a17a947bd
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-16 21:57:33 +0900

    add urlscan

--- osiete_osint/apps/service/admin.py
+++ osiete_osint/apps/service/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from .models import (DataList, OsintSearchHistory, Service, VtComments, 
-                    VtSummary,)
+from .models import (DataList, OsintSearchHistory, Service, UrlScan,
+                    VtComments, VtSummary,)
 
 # Register your models here.
 
 admin.site.register(DataList)
 admin.site.register(OsintSearchHistory)
 admin.site.register(Service)
+admin.site.register(UrlScan)
 admin.site.register(VtComments)
 admin.site.register(VtSummary)
