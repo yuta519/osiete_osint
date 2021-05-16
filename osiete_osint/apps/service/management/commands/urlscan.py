9605f046b2106fb070d661da6c57e1d074bb6c9b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-16 22:30:51 +0900

    add return type

--- osiete_osint/apps/service/management/commands/urlscan.py
+++ osiete_osint/apps/service/management/commands/urlscan.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('targets', nargs='+', type=str)
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **kwargs) -> str:
         urlscan = UrlScanClient()
         for target in kwargs['targets']:
             response = urlscan.fetch_domain_detail(target)
