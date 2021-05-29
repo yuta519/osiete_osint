31c3a286875836e64d824cee5ee051db7a3359f7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-30 00:27:14 +0900

    get DataList

--- osiete_osint/apps/service/management/commands/updateOsints.py
+++ osiete_osint/apps/service/management/commands/updateOsints.py
@@ -2,8 +2,15 @@ from django.core.management.base import BaseCommand
 
 from osiete_osint.apps.service.models import (DataList, UrlScan, VtSummary)
 
-class updateOsints(BaseCommand):
+class Command(BaseCommand):
     help = 'Update all OSINTS in database.'
 
+    def __init__(self):
+        self.osints = DataList.objects.all()
+
     def update_osint_of_vt(self):
+        print(self.osints)
+
+    def handle(self, *args, **kwargs) -> None:
+        self.update_osint_of_vt()
         pass
