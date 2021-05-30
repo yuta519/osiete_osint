accef5e806656adec4901b71a296c0293598cdcf
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-30 17:30:38 +0900

    work as cli

--- osiete_osint/apps/service/management/commands/updateOsints.py
+++ osiete_osint/apps/service/management/commands/updateOsints.py
@@ -1,15 +1,15 @@
 from django.core.management.base import BaseCommand
 
-from osiete_osint.apps.service.models import (DataList, UrlScan, VtSummary)
+from osiete_osint.apps.service.client import UrlScanClient, VirusTotalClient 
+
 
 class Command(BaseCommand):
     help = 'Update all OSINTS in database.'
 
-    def __init__(self):
-        self.osints = DataList.objects.all()
-
     def update_osint_of_vt(self):
-        print(self.osints)
+        vtclient = VirusTotalClient()
+        vtclient.update_vtrisk()
+        print('===================')
 
     def handle(self, *args, **kwargs) -> None:
         self.update_osint_of_vt()
